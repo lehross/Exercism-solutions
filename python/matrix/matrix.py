b878c92d4900b8959a14035781c12a291fcea1e3
Letícia <leticia.rossaneis@gmail.com>
2021-07-27 20:12:53 -0300

    matrix update

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,10 +1,10 @@
 class Matrix:
-    def __init__(self, matrix_string):
+    def __init__(self, matrix_string: str) -> None:
         # split the rows, turn each char into int and split each row
-        self.data = [[int(i) for i in row.split()] for row in matrix_string.split("\n")]
+        self.data = [[int(i) for i in row.split()] for row in matrix_string.splitlines()]
 
-    def row(self, index):
+    def row(self, index: int) -> list:
         return self.data[index - 1]
 
-    def column(self, index):
+    def column(self, index: int) -> list:
         return [row[index - 1] for row in self.data]
